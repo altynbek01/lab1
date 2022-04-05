5dc4ca8279e4c6fe019a679bdfa662f94cbe5d72
Altynbek <altynbek01@gmail.com>
2022-04-06 02:25:05 +0600

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
